a1f04f053fe587f0674b3a7a2c61bcb88e578e67
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-06-06 22:03:29 -0500

    dependencias iniciales

---
+++ src/main/java/com/isai/demo_api_con_mongodb/DemoApiConMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demo_api_con_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiConMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiConMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/demo_api_con_mongodb/DemoApiConMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demo_api_con_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiConMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
